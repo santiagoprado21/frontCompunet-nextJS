2649b14b383322eb22dec9e6f3fe0ea7f2347af1
santiagoprado21 <santiagoprado435@gmail.com>
2024-11-04 13:42:27 -0500

    title home done

--- online-store/src/app/(shop)/layout.tsx
+++ online-store/src/app/(shop)/layout.tsx
@@ -8,7 +8,10 @@ export default function ShopLayout({
     return (
         <main className="min-h-screen">
             <TopMenu/>
+            <div className="px-0 sm:px-10">
             {children}
+            </div>
+            
         </main>
     )
 }
--- online-store/src/app/(shop)/page.tsx
+++ online-store/src/app/(shop)/page.tsx
@@ -1,16 +1,13 @@
-import { TopMenu } from "@/components/ui/top-menu/TopMenu";
-import { titleFont } from "@/config/fonts";
-import Image from "next/image";
+import { Title } from "@/components";
 
 export default function Home() {
   return (
-    <div className="">
-      <div className="">
-        <h1>Hola mundo</h1>
-        <h1 className={titleFont.className}>Hola mundo</h1>
-      </div>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer>
-    </div>
+    <>
+      <Title 
+      title="Tienda"
+      subtitle="Todos los productos"
+      className="mb-2"
+      />
+    </>
   );
 }
