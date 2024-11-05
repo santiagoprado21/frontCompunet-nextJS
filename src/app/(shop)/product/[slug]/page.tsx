d910674a95defb17d5a4c3c021aea34d4ffe595c
Juan D. Lora <juan.lora1@u.icesi.edu.co>
2024-11-05 00:12:06 -0500

    show a product by id (remain api connection)

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,53 @@
-export default function () {
-    return (
-        <div>
-            <h1>Product Page</h1>
-        </div>
-    )
+//product/<id>
+import { GetServerSideProps } from 'next';
+import { redirect } from 'next/navigation';
+
+interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
 }
+
+interface ProductPageProps {
+    product: Product | null;
+    //will receive
+}
+
+function ProductPage({ product }: ProductPageProps) {
+  
+  // TODO change this to just appear when loading. It could't be "!product"
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+  
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+
+  return (
+    <div>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p>Price: ${product.price}</p>
+    </div>
+  );
+}
+
+async function getServerSideProps(context: GetServerSideProps) {
+    //TO DO this is not being used
+    const { id } = context.arguments;
+  
+  try {
+    const response = await fetch(`https://API-back /products/${id}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch product');
+    }
+    const product = await response.json();
+    return { props: { product } };
+  } catch (error) {
+    return { props: { product: null } };
+  }
+}
+
+export default ProductPage;
